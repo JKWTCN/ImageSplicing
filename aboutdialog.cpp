41409a759de6b95126fad6ecbdcfa1d5d4e5891e
JKWTCN <jkwtcn@icloud.com>
2025-12-21 23:26:14 +0800

    feat:增加关于和 cmakelist 版本挂钩功能;

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -1,10 +1,14 @@
 #include "aboutdialog.h"
 #include "ui_aboutdialog.h"
+#include "version.h"
 
 AboutDialog::AboutDialog(QWidget *parent) : QDialog(parent),
                                             ui(new Ui::AboutDialog)
 {
     ui->setupUi(this);
+
+    // 设置动态版本号
+    ui->versionLabel->setText(QString("版本 %1.%2.%3").arg(APP_VERSION_MAJOR).arg(APP_VERSION_MINOR).arg(APP_VERSION_PATCH));
 }
 
 AboutDialog::~AboutDialog()
